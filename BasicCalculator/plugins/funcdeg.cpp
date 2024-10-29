87225e94ad565ca9183fbfcf505153a32e49280c
Kt280174 <kh280174@gmail.com>
2024-10-29 21:52:15 +0300

    fix lab 1

--- BasicCalculator/plugins/funcdeg.cpp
+++
@@ -1,8 +0,0 @@
-// plugins/math_power.cpp
-#include <cmath>
-
-extern "C" {
-    __declspec(dllexport) double power_function(double base, double exponent) {
-        return pow(base, exponent);
-    }
-}
